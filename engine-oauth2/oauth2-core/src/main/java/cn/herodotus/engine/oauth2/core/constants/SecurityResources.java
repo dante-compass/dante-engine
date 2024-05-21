cd8103330d8165dd4fc3e9ae5fd859626201e116
码匠君 <pointer_v@qq.com>
2024-05-21 09:29:31 +0800

    [修复] 修复日志代码引入类不规范问题。fix: #I9QR01 (ISSUED by 杨运交) [修复] 修复 Webjars JQuery 重复引入造成等登录页面单独访问无法加载问题。fix: #I9QRT0 (ISSUED by 杨运交) [修复] 修复授权码模式开启确认页面显示 401 无权限问题。fix: #I9QS1H (ISSUED by 杨运交)

--- engine-oauth2/oauth2-core/src/main/java/cn/herodotus/engine/oauth2/core/constants/SecurityResources.java
+++ engine-oauth2/oauth2-core/src/main/java/cn/herodotus/engine/oauth2/core/constants/SecurityResources.java
@@ -16,6 +16,7 @@
 
 package cn.herodotus.engine.oauth2.core.constants;
 
+import cn.herodotus.engine.assistant.definition.constants.DefaultConstants;
 import com.google.common.collect.Lists;
 
 import java.util.List;
@@ -44,5 +45,5 @@ public class SecurityResources {
             "/favicon.ico");
     public static final List<String> DEFAULT_PERMIT_ALL_RESOURCES = Lists.newArrayList("/open/**", "/stomp/ws", "/oauth2/sign-out", "/login*");
 
-    public static final List<String> DEFAULT_HAS_AUTHENTICATED_RESOURCES = Lists.newArrayList("/engine-rest/**");
+    public static final List<String> DEFAULT_HAS_AUTHENTICATED_RESOURCES = Lists.newArrayList("/engine-rest/**", DefaultConstants.DEVICE_VERIFICATION_SUCCESS_URI, DefaultConstants.AUTHORIZATION_CONSENT_URI);
 }
